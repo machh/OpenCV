0e768e46121d53bd85a16c06b16e3660cbe824d2
haige <haige521@126.com>
2021-02-09 13:24:28 +0800

    Create demo.cpp

---
+++ demo.cpp
@@ -0,0 +1,19 @@
+include <opencv2\opencv.hpp> 
+#include <iostream>
+
+using namespace std;
+using namespace cv;   
+
+int main(int argc, char ** argv) 
+{
+	Mat img;   
+	img = imread("d:/opencv/cat.png");  
+	if (img.empty())  
+	{
+		cout << "file error!" << endl;
+		return -1;
+	}
+	imshow("test", img);  
+	waitKey(0);  
+	return 0;   
+}
